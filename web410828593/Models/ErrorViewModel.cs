108bfb31a13eef4a5acd8ec4981d2219dc76ebac
邱文也 <wayne900204@gmail.com>
2021-05-26 21:13:07 +0800

    [Feature]新增HW1

---
+++ web410828593/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web410828593.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
